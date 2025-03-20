82bf42b993336eb1633a1372bd316f2dcd8d162a
Aurel <koussemonaurel@gmail.com>
2025-03-21 00:39:28 +0100

    HeHe

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
         elementACacher.style.display = this.open ? "none" : "inline";
     });
 });
+
+/* JavaScript pour le menu mobile */
+const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+const mainNav = document.querySelector('.main-nav');
+
+mobileMenuToggle.addEventListener('click', function() {
+    mainNav.classList.toggle('active');
+    if (mainNav.classList.contains('active')) {
+        mobileMenuToggle.style.display = 'none'; // Masque le bouton burger
+    } else {
+        mobileMenuToggle.style.display = 'block'; // Affiche le bouton burger
+    }
+});
